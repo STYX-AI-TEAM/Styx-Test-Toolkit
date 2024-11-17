7b1edb2978f1fa16c8ef9ebc64912e522501afe7
Anirudh <77240291+Anirudh3167@users.noreply.github.com>
2024-11-17 13:51:38 +0530

    Update and rename sample.py to main.py

---
+++ models/main.py
@@ -0,0 +1,34 @@
+import torch
+from transformers import pipeline
+
+class StyxModels:
+  endpoint = None
+  model = None
+  model_map = {
+    "gpt2" : "openai-community/gpt2"
+  }
+  def __init__(self, model = None, endpoint = None, api_key = None):
+    if model is not None:
+      device = 0 if torch.cuda.is_available() else -1
+      if model in self.model_map:
+        model = self.model_map[model]
+      self.model = pipeline("text-generation", model=model,device=device)
+    self.url = endpoint
+    self.headers = {
+        "Authorization": f"Bearer {api_key}" if api_key else "",
+        "Content-Type": "application/json",
+    }
+    
+  def generate(self, prompt = None, context = None, max_new_tokens = 100, num_return_sequences = 1):
+    if self.model is not None:
+      return model(prompt, max_new_tokens = max_new_tokens, num_return_sequences)
+    body = {
+            "messages": [{"role": "user", "content": prompt}] if context is None else context,
+            "temperature": 0.5,
+            "max_tokens": 2500,
+            "top_p": 0.7,
+            "stream": False
+        }
+    res = json.loads(requests.post(url=self.url, headers=self.headers, json=body).text)
+    return res['choices'][-1]['message']['content']
+
--- models/sample.py
+++
@@ -1,8 +0,0 @@
-import torch
-from transformers import pipeline
-
-
-def gpt2():
-  device = 0 if torch.cuda.is_available() else -1
-
-  return pipeline("text-generation", model="openai-community/gpt2",device=device)
