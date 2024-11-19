afac2fa13f363f294781d4843c8c40d7b1569ca2
Anirudh3167 <77240291+Anirudh3167@users.noreply.github.com>
2024-11-19 22:31:03 +0530

    Bug: StyxModels model variable updated

--- models/main.py
+++ models/main.py
@@ -5,9 +5,9 @@ class StyxModels:
   model_map = {
     "gpt 2" : "openai-community/gpt2",
     "llama 3.2" : "meta-llama/Llama-3.2-1B",
-    "Gemma 2" : "google/gemma-2-2b-it",
-    
+    "Gemma 2" : "google/gemma-2-2b-it", 
   }
+  model = None
   def __init__(self, model = None, endpoint = None, api_key = None):
     if model is not None:
       device = 0 if torch.cuda.is_available() else -1
